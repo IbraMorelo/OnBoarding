51912e89e3dce6b7a532f8ba1b42bba5be893c70
Ayda Acuña <marioalejandro@Dark-Side.local>
2018-02-01 15:41:32 -0500

    Initial Commit

---
+++ OnBoarding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnBoarding
+//
+//  Created by Ibra Morelo on 1/02/18.
+//  Copyright © 2018 Ibra Morelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
